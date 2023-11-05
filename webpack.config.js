4bfb0cfdab0d65a671bc7ca4398e313c4b94daa6
rijvan <rijvan.mohammad@indovations.in>
2023-11-05 21:31:22 +0530

    set up

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "inline-source-map",
+
+  entry: ["webpack-hot-middleware/client", "./client/index.js"],
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+  },
+
+  model: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: { node: "current" } }],
+              ["@babel/preset-react", { targets: { node: "current" } }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Your custom title",
+      template: "./client/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development"),
+      },
+    }), 
+  ],
+};
